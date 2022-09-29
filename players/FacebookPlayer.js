1f9e7993d77bb44c401886a95e95e3cc66f34069
Waleed <waleed.i@dplit.com>
2022-09-29 15:29:48 +0500

    Poster added

--- players/FacebookPlayer.js
+++ players/FacebookPlayer.js
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import withDefaultScreenContainer from '../DefaultScreenContainer';
 import { toTimeView } from '../utils';
 import Overlay from '../controls/Overlay';
@@ -15,55 +15,78 @@ import { useInternalCtx } from '../InternalCtx';
 import Text from '../controls/Text';
 import VolumeToggle from '../controls/VolumeToggle';
 import { useVideoCtx } from '../ScreenContainer';
+import FastImage from 'react-native-fast-image';
+import Ionicons from 'react-native-vector-icons/dist/Ionicons';
 const TimePlayed = () => {
-    const { mutableState } = useInternalCtx();
-    return (<Text style={{ width: 80 }}>{toTimeView(mutableState.currentTime)}</Text>);
+  const { mutableState } = useInternalCtx();
+  return (<Text style={{ width: 80 }}>{toTimeView(mutableState.currentTime)}</Text>);
+};
+const Poster = () => {
+  const { paused,mutableState } = useInternalCtx();
+  console.log('mutableState.currentTime',mutableState.currentTime)
+  return (
+    (paused && mutableState.currentTime <= 0.1) &&
+    <FastImage
+      source={{
+        uri: "https://miraclemorning-media.s3.amazonaws.com/images/thumbnail/Rennee.jpg"
+      }}
+      resizeMode="cover"
+      style={{
+        width: "100%",
+        height: "100%",
+        //zIndex: 9999999,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    />
+  );
 };
 const TimeLeft = () => {
-    const { mutableState, duration } = useInternalCtx();
-    return (<Text style={{ width: 80 }}>
-      -{toTimeView(duration - mutableState.currentTime)}
-    </Text>);
+  const { mutableState, duration } = useInternalCtx();
+  return (<Text style={{ width: 80 }}>
+    -{toTimeView(duration - mutableState.currentTime)}
+  </Text>);
 };
 const FacebookPlayer = forwardRef(({ mode, initialPaused = false, initialMuted = false, aspectRatio = 'landscape', customIcon, renderToolbar, ...props }, ref) => {
-    const { fullscreen } = useVideoCtx();
-    return (<VideoContainer mode={mode} aspectRatio={aspectRatio} initialPaused={initialPaused} initialMuted={initialMuted}>
-        
-        <RNVideo /* style={{ width: '100%', height: '100%' }} */  {...props} ref={ref}/>
-        <Overlay>
-          <Center>
-            <Replay>{customIcon?.replayIcon}</Replay>
-            <PlayPauseRefresh {...customIcon}/>
-            <Forward>{customIcon?.forwardIcon}</Forward>
-          </Center>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            {renderToolbar?.(fullscreen)}
-            <View style={{ flex: 1 }}/>
-            <View style={{
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: fullscreen ? 30 : 30,
-    }}>
-              <TimePlayed />
-              <EnhancedSeeker mode={mode} config={{
-        buttonColor: '#fff',
-        filledColor: '#1877f2',
-        barHeight: 4,
-        initialButtonSize: 18,
-        touchedButtonSize: 18,
-    }} styles={{
-        seeker: { flex: 1 },
-        buffer: { borderRadius: 10 },
-        duration: { borderRadius: 10 },
-        played: { borderRadius: 10 },
-    }}/>
-              <TimeLeft />
-              <VolumeToggle {...customIcon}/>
-              <FullscreenToggle {...customIcon}/>
-            </View>
-          </View>
-        </Overlay>
-      </VideoContainer>);
+  const { fullscreen } = useVideoCtx();
+  return (<VideoContainer mode={mode} aspectRatio={aspectRatio} initialPaused={initialPaused} initialMuted={initialMuted}>
+
+    <RNVideo /* style={{ width: '100%', height: '100%' }} */  {...props} ref={ref} />
+    <Poster />
+    <Overlay>
+      <Center>
+        <Replay>{customIcon?.replayIcon}</Replay>
+        <PlayPauseRefresh {...customIcon} />
+        <Forward>{customIcon?.forwardIcon}</Forward>
+      </Center>
+      <View style={{ flex: 1, alignSelf: 'stretch' }}>
+        {renderToolbar?.(fullscreen)}
+        <View style={{ flex: 1 }} />
+        <View style={{
+          width: '100%',
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginBottom: fullscreen ? 30 : 30,
+        }}>
+          <TimePlayed />
+          <EnhancedSeeker mode={mode} config={{
+            buttonColor: '#fff',
+            filledColor: '#1877f2',
+            barHeight: 4,
+            initialButtonSize: 18,
+            touchedButtonSize: 18,
+          }} styles={{
+            seeker: { flex: 1 },
+            buffer: { borderRadius: 10 },
+            duration: { borderRadius: 10 },
+            played: { borderRadius: 10 },
+          }} />
+          <TimeLeft />
+          <VolumeToggle {...customIcon} />
+          <FullscreenToggle {...customIcon} />
+        </View>
+      </View>
+    </Overlay>
+  </VideoContainer>);
 });
 export default withDefaultScreenContainer(FacebookPlayer);
